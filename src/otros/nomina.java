12c54243cf2408a0f118ceeebf58bf2fab584365
JuanDR-19 <juanmanuelduranrueda@gmail.com>
2022-05-17 17:47:09 -0500

    se crearon los array de nomina

--- src/otros/nomina.java
+++ src/otros/nomina.java
@@ -1,7 +1,15 @@
 package otros;
 
+import Entidades.Empleado;
+import Entidades.Monitor;
+import Entidades.Profesor;
+import java.util.ArrayList;
+
 public class nomina {
 
+	ArrayList<Empleado> empleados= new ArrayList<Empleado>();
+	
+	
 	public nomina() {
 		// TODO Auto-generated constructor stub
 	}
